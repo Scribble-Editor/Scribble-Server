46ecec58ce44140e862a8e7c1b619296cce45c52
Logan Crandall <crandall.logan@gmail.com>
2019-11-19 19:28:22 -0500

    add account/logout route to delete auth tokens to be regenerated when logged into again (#5)

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login, register, sample_api
+from .views import login, register, logout, sample_api
 
 urlpatterns = [
   path('login', login),
   path('register', register),
-  path('sample-api', sample_api)
+  path('logout', logout),
+  path('sample-api', sample_api),
 ]
