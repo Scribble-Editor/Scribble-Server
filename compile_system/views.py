0250a12ae1b98a65cd8afc69939e35f47ab84950
Patrick M. Howard <pmontgomeryhoward@gmail.com>
2019-11-22 04:09:41 -0500

    Add whitespace for consistent styling

--- compile_system/views.py
+++ compile_system/views.py
@@ -2,14 +2,17 @@ from compile_system.statement import constructCompileStatement, constructInterpr
 from compile_system.writeFile import writeFile
 from compile_system.models import Scribblet
 
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_409_CONFLICT
 from rest_framework.response import Response
 
-import requests
+import requests   
 
 def getItemsFromReq(request):
+  
     name = request.get("name") #Scribblet name
     target = request.get("target") #Operating System/Architecture (e.g. Win64, Win32, Linux)
     language = request.get("lang") #Language in the form of file extension (e.g. cpp, c, rb, py)
